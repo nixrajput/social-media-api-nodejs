67dbe71c7804c242f13fb38e2b278cbcdc78b87c
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-09-13 16:51:43 +0530

    hashtag implemented and searching algorithms improved

--- src/modules/user/controllers/user/getUserPosts.js
+++ src/modules/user/controllers/user/getUserPosts.js
@@ -1,3 +1,4 @@
+import ResponseMessages from "../../../../contants/responseMessages.js";
 import catchAsyncError from "../../../../helpers/catchAsyncError.js";
 import ErrorHandler from "../../../../helpers/errorHandler.js";
 import models from "../../../../models/index.js";
@@ -7,10 +8,10 @@ import utility from "../../../../utils/utility.js";
 
 const getUserPosts = catchAsyncError(async (req, res, next) => {
     const user = await models.User.findById(req.query.id)
-        .select(["_id", "accountPrivacy",]);
+        .select("_id accountPrivacy posts postsCount");
 
     if (!user) {
-        return next(new ErrorHandler("user not found", 404));
+        return next(new ErrorHandler(ResponseMessages.USER_NOT_FOUND, 404));
     }
 
     let currentPage = parseInt(req.query.page) || 1;
@@ -20,31 +21,7 @@ const getUserPosts = catchAsyncError(async (req, res, next) => {
         const followingStatus = await utility.getFollowingStatus(req.user, user._id);
         const isSameUser = await utility.checkIfSameUser(req.user, user._id);
         if (followingStatus === "following" || isSameUser) {
-            const posts = await models.Post.find({ owner: user._id })
-                .sort({ createdAt: -1 });
-
-            const ownerData = await utility.getOwnerData(user._id, req.user);
-
-            const postsResults = [];
-
-            for (let i = 0; i < posts.length; i++) {
-                const post = posts[i];
-
-                const isLiked = await utility.checkIfPostLiked(post._id, req.user);
-
-                const postData = {};
-                postData._id = post._id;
-                postData.caption = post.caption;
-                postData.mediaFiles = post.mediaFiles;
-                postData.owner = ownerData;
-                postData.likesCount = post.likesCount;
-                postData.commentsCount = post.commentsCount;
-                postData.isLiked = isLiked;
-                postData.postStatus = post.postStatus;
-                postData.createdAt = post.createdAt;
-
-                postsResults.push(postData);
-            }
+            const posts = user.posts;
 
             let totalPosts = posts.length;
             let totalPages = Math.ceil(totalPosts / limit);
@@ -87,7 +64,19 @@ const getUserPosts = catchAsyncError(async (req, res, next) => {
                 nextPage = `${baseUrl}?page=${nextPageIndex}&limit=${limit}`;
             }
 
-            const results = postsResults.slice(skip, skip + limit);
+            const slicedPosts = posts.slice(skip, skip + limit);
+
+            const results = [];
+
+            for (let postId of slicedPosts) {
+                const postData = await utility.getPostData(postId, req.user);
+
+                results.push(postData);
+            }
+
+            results.sort((a, b) => {
+                return new Date(b.createdAt) - new Date(a.createdAt);
+            });
 
             res.status(200).json({
                 success: true,
@@ -115,31 +104,7 @@ const getUserPosts = catchAsyncError(async (req, res, next) => {
             });
         }
     } else {
-        const posts = await models.Post.find({ owner: user._id })
-            .sort({ createdAt: -1 });
-
-        const ownerData = await utility.getOwnerData(user._id, req.user);
-
-        const postsResults = [];
-
-        for (let i = 0; i < posts.length; i++) {
-            const post = posts[i];
-
-            const isLiked = await utility.checkIfPostLiked(post._id, req.user);
-
-            const postData = {};
-            postData._id = post._id;
-            postData.caption = post.caption;
-            postData.mediaFiles = post.mediaFiles;
-            postData.owner = ownerData;
-            postData.likesCount = post.likesCount;
-            postData.commentsCount = post.commentsCount;
-            postData.isLiked = isLiked;
-            postData.postStatus = post.postStatus;
-            postData.createdAt = post.createdAt;
-
-            postsResults.push(postData);
-        }
+        const posts = user.posts;
 
         let totalPosts = posts.length;
         let totalPages = Math.ceil(totalPosts / limit);
@@ -182,7 +147,19 @@ const getUserPosts = catchAsyncError(async (req, res, next) => {
             nextPage = `${baseUrl}?page=${nextPageIndex}&limit=${limit}`;
         }
 
-        const results = postsResults.slice(skip, skip + limit);
+        const slicedPosts = posts.slice(skip, skip + limit);
+
+        const results = [];
+
+        for (let postId of slicedPosts) {
+            const postData = await utility.getPostData(postId, req.user);
+
+            results.push(postData);
+        }
+
+        results.sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt);
+        });
 
         res.status(200).json({
             success: true,
