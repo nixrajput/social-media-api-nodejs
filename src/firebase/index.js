e226480f027264e04aaecec1cd8f5107e498728e
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-10-12 07:36:01 +0530

    bug fixes and update changes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -7,11 +7,13 @@ if (process.env.NODE_ENV !== "production") {
     });
 }
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "");
+
 const serviceAccount = {
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: privateKey,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
