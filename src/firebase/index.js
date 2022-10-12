d03d90e806662a41e8ed8aca69bc7605d14374af
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-10-12 07:24:23 +0530

    bug fixes and update changes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -20,6 +20,8 @@ const serviceAccount = {
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 };
 
+console.log(serviceAccount);
+
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 })
